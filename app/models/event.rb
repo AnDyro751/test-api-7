980d4e85599a6990483daf8ce7b063107d6611f1
Angel Mendez <angelmendezz751@gmail.com>
2021-05-14 22:58:18 -0500

    Me quedan 4 minutos, por si no lo acabo, usaré un counter cache para hacer el conteo y el ordenado de los actors en GET /actors

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
   alias_attribute :type, :kind
-  belongs_to :actor, dependent: :destroy
+  belongs_to :actor, dependent: :destroy, counter_cache: true
   belongs_to :repository, dependent: :destroy
 end
